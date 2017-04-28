05b259988eaab829570356072a62c0e70c22fc36
Nicholai Spinnangr <chillmonger@gmail.com>
2017-04-28 15:04:07 +0200

    Commit nr. empty

--- app/src/main/java/com/d24/android/pictogramapp/PickPictogramActivity.java
+++ app/src/main/java/com/d24/android/pictogramapp/PickPictogramActivity.java
@@ -1,13 +1,16 @@
 package com.d24.android.pictogramapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class SceneActivity extends AppCompatActivity {
+public class PickPictogramActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,5 @@ public class SceneActivity extends AppCompatActivity {
         });
     }
 
-
     // 1. LAGE EN '+' KNAPP FOR PIKTOGRAM
-    // 2. LISTE AV PIKTOGRAM (FRAGMENT, ACTIVITY)
-
 }
