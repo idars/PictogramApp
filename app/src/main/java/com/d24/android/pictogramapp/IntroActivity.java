a83e7d5a725be7496144e496de87e39516b4ccae
Idar Syslak <idasys1@outlook.com>
2017-04-28 15:50:03 +0200

    Rename TutorialActivity, replace SceneActivity and change a few icons

--- app/src/main/java/com/d24/android/pictogramapp/IntroActivity.java
+++ app/src/main/java/com/d24/android/pictogramapp/IntroActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SceneActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
