7cb2f8cd9f1123f1c1a60f2baa76587714d53147
Idar Syslak <idasys1@outlook.com>
2017-04-28 15:56:52 +0200

    Rename connections and remove ActionBar from IntroActivity

--- app/src/main/java/com/d24/android/pictogramapp/IntroActivity.java
+++ app/src/main/java/com/d24/android/pictogramapp/IntroActivity.java
@@ -12,18 +12,7 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_intro);
     }
 
 
