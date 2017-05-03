16a015cc6bedd6299c67157fcfdf5daaffcdb702
Idar Syslak <idasys1@outlook.com>
2017-05-03 18:24:22 +0200

    Configure tool fragment with callbacks , update translations

--- app/src/main/java/com/d24/android/pictogramapp/ui/ToolFragment.java
+++ app/src/main/java/com/d24/android/pictogramapp/ui/ToolFragment.java
@@ -13,76 +13,74 @@ import com.d24.android.pictogramapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ToolFragment.OnFragmentInteractionListener} interface
+ * {@link ToolFragment.OnToolSelectedListener} interface
  * to handle interaction events.
  * Use the {@link ToolFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ToolFragment extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
+public class ToolFragment extends Fragment implements View.OnClickListener {
 
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
-
-	private OnFragmentInteractionListener mListener;
+	private OnToolSelectedListener mListener;
 
 	public ToolFragment() {
 		// Required empty public constructor
 	}
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment ToolFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
-	public static ToolFragment newInstance(String param1, String param2) {
-		ToolFragment fragment = new ToolFragment();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
-		return fragment;
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+	public static ToolFragment newInstance() {
+		return new ToolFragment();
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_tool, container, false);
+		View view = inflater.inflate(R.layout.fragment_tool, container, false);
+
+		view.findViewById(R.id.button_add).setOnClickListener(this);
+		view.findViewById(R.id.button_background).setOnClickListener(this);
+		view.findViewById(R.id.button_undo).setOnClickListener(this);
+		view.findViewById(R.id.button_redo).setOnClickListener(this);
+		view.findViewById(R.id.button_info).setOnClickListener(this);
+
+		return view;
 	}
 
-	// TODO: Rename method, update argument and hook method into UI event
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onFragmentInteraction(uri);
+	@Override
+	public void onClick(View view) {
+		if (mListener == null) {
+			return;
+		}
+
+		// Use the corresponding callback depending on which button is being pressed
+		switch (view.getId()) {
+			case R.id.button_add:
+				mListener.onAddButtonSelected(view);
+				break;
+			case R.id.button_background:
+				mListener.onBackgroundButtonSelected(view);
+				break;
+			case R.id.button_undo:
+				mListener.onUndoButtonSelected(view);
+				break;
+			case R.id.button_redo:
+				mListener.onRedoButtonSelected(view);
+				break;
+			case R.id.button_info:
+				mListener.onInfoButtonSelected(view);
+				break;
+			default:
+				break;
 		}
 	}
 
 	@Override
 	public void onAttach(Context context) {
 		super.onAttach(context);
-		if (context instanceof OnFragmentInteractionListener) {
-			mListener = (OnFragmentInteractionListener) context;
+		if (context instanceof OnToolSelectedListener) {
+			mListener = (OnToolSelectedListener) context;
 		} else {
 			throw new RuntimeException(context.toString()
-					+ " must implement OnFragmentInteractionListener");
+					+ " must implement OnToolSelectedListener");
 		}
 	}
 
@@ -102,8 +100,11 @@ public class ToolFragment extends Fragment {
 	 * "http://developer.android.com/training/basics/fragments/communicating.html"
 	 * >Communicating with Other Fragments</a> for more information.
 	 */
-	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
-		void onFragmentInteraction(Uri uri);
+	public interface OnToolSelectedListener {
+		void onAddButtonSelected(View v);
+		void onBackgroundButtonSelected(View v);
+		void onUndoButtonSelected(View v);
+		void onRedoButtonSelected(View v);
+		void onInfoButtonSelected(View v);
 	}
 }
