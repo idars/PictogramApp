843b4dcc9a636beefc4f65c4c26b36b888e8c355
Nicholai Spinnangr <chillmonger@gmail.com>
2017-05-03 17:21:18 +0200

    Attempting branching out

--- app/src/main/java/com/d24/android/pictogramapp/StagingActivity.java
+++ app/src/main/java/com/d24/android/pictogramapp/StagingActivity.java
@@ -27,82 +27,9 @@ import com.d24.android.pictogramapp.fragment.SelectingFragment;
 public class StagingActivity extends AppCompatActivity {
 
 	private ImageView img;
-	private ViewGroup rootLayout;
-	private int _xDelta;
-	private int _yDelta;
+
 	EditingFragment editingFragagment;
 	SelectingFragment selectingFragagment;
-
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-
-	private static final boolean AUTO_HIDE = true;
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * Some older devices needs a small delay between UI widget updates
-	 * and a change of the status and navigation bar.
-	 */
-	private static final int UI_ANIMATION_DELAY = 300;
-	private final Handler mHideHandler = new Handler();
-	private View mContentView;
-	private final Runnable mHidePart2Runnable = new Runnable() {
-		@SuppressLint("InlinedApi")
-		@Override
-		public void run() {
-			// Delayed removal of status and navigation bar
-
-			// Note that some of these constants are new as of API 16 (Jelly Bean)
-			// and API 19 (KitKat). It is safe to use them, as they are inlined
-			// at compile-time and do nothing on earlier devices.
-			mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-		}
-	};
-	private View mControlsView;
-	private final Runnable mShowPart2Runnable = new Runnable() {
-		@Override
-		public void run() {
-			// Delayed display of UI elements
-			ActionBar actionBar = getSupportActionBar();
-			if (actionBar != null) {
-				actionBar.show();
-			}
-			mControlsView.setVisibility(View.VISIBLE);
-		}
-	};
-	private boolean mVisible;
-	private final Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			hide();
-		}
-	};
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -116,31 +43,8 @@ public class StagingActivity extends AppCompatActivity {
 		}
 
 		/* TODO, MOVE TO FRAGMENT */
-		/*mVisible = true;
-		mControlsView = findViewById(R.id.fullscreen_content_controls);
-		mContentView = findViewById(R.id.fullscreen_content);
 
 
-		// Set up the user interaction to manually show or hide the system UI.
-		mContentView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				toggle();
-			}
-		});
-
-		rootLayout = (ViewGroup) findViewById(R.id.view_root);
-		img = (ImageView) rootLayout.findViewById(R.id.fullscreen_content);
-
-		RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
-		img.setLayoutParams(layoutParams);
-		img.setOnTouchListener(new ChoiceTouchListener());
-
-		// Upon interacting with UI controls, delay any scheduled hide()
-		// operations to prevent the jarring behavior of controls going away
-		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-*/
 		// TODO, Over here nich! Start transaction
 		editingFragagment	=new EditingFragment();//create the fragment instance for the top fragment
 		selectingFragagment =new SelectingFragment();//create the fragment instance for the bottom fragment
@@ -150,21 +54,17 @@ public class StagingActivity extends AppCompatActivity {
 		// TODO, keep FragmentManager as class variable?, it is used in selectClick
 		FragmentManager manager=getSupportFragmentManager();//create an instance of fragment manager
 		FragmentTransaction transaction=manager.beginTransaction();	//create an instance of Fragment-transaction
-
 		transaction.add(R.id.fragment_editing, editingFragagment).addToBackStack(editingFragmentTag);
-		transaction.add(R.id.fragment_selection, selectingFragagment).addToBackStack(selectingFragmentTag);
-
+		transaction.add(R.id.fragment_selection, selectingFragagment);//.addToBackStack(selectingFragmentTag);
 		transaction.hide(selectingFragagment);
+
 		transaction.commit();
-
-
-
 
 	}
 
 
 	public void selectClick(View view){
-
+		Log.i("D-bug", "BUTTON CLICKED");
 		int idTop = R.id.fragment_editing;
 		int idMid = R.id.fragment_selection;
 
@@ -175,25 +75,8 @@ public class StagingActivity extends AppCompatActivity {
 		transaction.commit();
 	}
 
-
-
-
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-        Log.i("D-bug", "onPostCreate");
-
-    }
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-        Log.i("D-bug", "onOptionsItemSelected");
 		int id = item.getItemId();
 		if (id == android.R.id.home) {
 			// This ID represents the Home or Up button.
@@ -203,88 +86,6 @@ public class StagingActivity extends AppCompatActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void toggle() {
-        Log.i("D-bug", "toggle");
 
-        if (mVisible) {
-			hide();
-		} else {
-			show();
-		}
-	}
 
-	private void hide() {
-        Log.i("D-bug", "hide");
-		//TODO, Hide peeps
-		/*
-        // Hide UI first
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			actionBar.hide();
-		}
-		mControlsView.setVisibility(View.GONE);
-		mVisible = false;
-
-		// Schedule a runnable to remove the status and navigation bar after a delay
-		mHideHandler.removeCallbacks(mShowPart2Runnable);
-		mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);*/
-	}
-
-	@SuppressLint("InlinedApi")
-	private void show() {
-		Log.i("D-bug", "show");
-
-		// Show the system bar
-		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-		mVisible = true;
-
-		// Schedule a runnable to display UI elements after a delay
-		mHideHandler.removeCallbacks(mHidePart2Runnable);
-		mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-	}
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		Log.i("D-bug", "delayedHide");
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
-
-	private final class ChoiceTouchListener implements View.OnTouchListener {
-
-		public boolean onTouch(View view, MotionEvent event) {
-			final int X = (int) event.getRawX();
-			final int Y = (int) event.getRawY();
-			switch (event.getAction() & MotionEvent.ACTION_MASK) {
-				case MotionEvent.ACTION_DOWN:
-					RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-					_xDelta = X - lParams.leftMargin;
-					_yDelta = Y - lParams.topMargin;
-					break;
-				case MotionEvent.ACTION_UP:
-					break;
-				case MotionEvent.ACTION_POINTER_DOWN:
-					break;
-				case MotionEvent.ACTION_POINTER_UP:
-					break;
-				case MotionEvent.ACTION_MOVE:
-					RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
-							.getLayoutParams();
-					layoutParams.leftMargin = X - _xDelta;
-					layoutParams.topMargin = Y - _yDelta;
-					//layoutParams.rightMargin = -250;
-					//layoutParams.bottomMargin = -250;
-					view.setLayoutParams(layoutParams);
-					break;
-			}
-			Log.i("D-bug", "invalidate");
-
-			rootLayout.invalidate();
-			return true;
-		}
-	}
 }
--- app/src/main/java/com/d24/android/pictogramapp/fragment/EditingFragment.java
+++ app/src/main/java/com/d24/android/pictogramapp/fragment/EditingFragment.java
@@ -6,64 +6,75 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.d24.android.pictogramapp.GridViewAdapter;
 import com.d24.android.pictogramapp.R;
+import com.d24.android.pictogramapp.StagingActivity;
 
 import java.util.ArrayList;
 
 public class EditingFragment extends Fragment {
 
     View view;
-    GridView gridView;
-    private GridViewAdapter gridAdapter;
-
+    private ViewGroup rootLayout;
+    ImageView imageView;
+    private int _xDelta;
+    private int _yDelta;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_stage_editing, container,false);
+        rootLayout = (ViewGroup) view.findViewById(R.id.view_root);
+        imageView = (ImageView)  view.findViewById(R.id.fullscreen_content);
 
-        view=inflater.inflate(R.layout.fragment_pictogram_select, container,false);
-        gridView = (GridView)  view.findViewById(R.id.gridView);
-        gridAdapter = new GridViewAdapter(getActivity(), R.layout.grid_item_layout, getData());
-        gridView.setAdapter(gridAdapter);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        imageView.setLayoutParams(layoutParams);
 
-        // Configure what happens when an item in the list is clicked
-        gridView.setClickable(true);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int i, long l) {
-                String text = "row id of the item clicked: " + 55;
-                Snackbar.make(view, text, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                /*
-                        Resources res = getResources();
-                        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-                        Drawable drawable = image_ids.getDrawable(l);
-                        (ImageView) flyttbarPiktogram.drawable.setImageDrawable(drawable);
-                 */
-            }
-        });
+        imageView.setOnTouchListener(new EditingFragment.ChoiceTouchListener());
 
         return view;
 
     }
+    private final class ChoiceTouchListener implements View.OnTouchListener {
 
-    // Prepare array data for gridview
-    private ArrayList<Drawable> getData() {
-        final ArrayList<Drawable> imageItems = new ArrayList<>();
-        Resources res = getResources();
-        TypedArray image_ids = res.obtainTypedArray(R.array.image_ids);
-
-        for (int i = 0; i < image_ids.length() ||i < 10; i++) {
-            Drawable drawable = image_ids.getDrawable(i);
-            imageItems.add(drawable);
+        public boolean onTouch(View view, MotionEvent event) {
+            Log.i("D-bug", "CHOICETOUCH");
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                            .getLayoutParams();
+                    layoutParams.leftMargin = X - _xDelta;
+                    layoutParams.topMargin = Y - _yDelta;
+                    //layoutParams.rightMargin = -250;
+                    //layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            rootLayout.invalidate();
+            return true;
         }
-        return imageItems;
     }
 }
