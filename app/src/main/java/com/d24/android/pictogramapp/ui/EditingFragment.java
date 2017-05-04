4d83c22d61ac7d1afa427e124b019b8580f56efe
marfur9 <marfur9@hotmail.com>
2017-05-04 19:45:01 +0200

    added stickerview on images, works, but has small canvas

--- app/src/main/java/com/d24/android/pictogramapp/ui/EditingFragment.java
+++ app/src/main/java/com/d24/android/pictogramapp/ui/EditingFragment.java
@@ -10,17 +10,19 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.d24.android.pictogramapp.R;
+import com.d24.android.pictogramapp.stickerview.StickerImageView;
 
 import java.util.ArrayList;
 
 public class EditingFragment extends Fragment {
 
     View view;
-    private ViewGroup rootLayout;
+    FrameLayout canvas;
     private int _xDelta;
     private int _yDelta;
 
@@ -28,7 +30,7 @@ public class EditingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_stage_editing, container,false);
-        rootLayout = (ViewGroup) view.findViewById(R.id.view_root);
+        canvas = (FrameLayout) view.findViewById(R.id.canvasView);
 
         return view;
 
@@ -44,44 +46,20 @@ public class EditingFragment extends Fragment {
 
         Drawable drawable = image_ids.getDrawable(intItemId);
 
-        ImageView newImg = new ImageView(getActivity());
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
-        newImg.setLayoutParams(layoutParams);
-        newImg.setOnTouchListener(new ChoiceTouchListener());
+        StickerImageView newImg = new StickerImageView(getActivity());
+        //RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        //newImg.setLayoutParams(layoutParams);
+        //newImg.setOnTouchListener(new ChoiceTouchListener());
         newImg.setImageDrawable(drawable);
-        rootLayout.addView(newImg);
+        canvas.addView(newImg);
 
     }
 
-    private final class ChoiceTouchListener implements View.OnTouchListener {
+    /*private final class ChoiceTouchListener implements View.OnTouchListener {
 
         public boolean onTouch(View view, MotionEvent event) {
-            final int X = (int) event.getRawX();
-            final int Y = (int) event.getRawY();
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN:
-                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                    _xDelta = X - lParams.leftMargin;
-                    _yDelta = Y - lParams.topMargin;
-                    break;
-                case MotionEvent.ACTION_UP:
-                    break;
-                case MotionEvent.ACTION_POINTER_DOWN:
-                    break;
-                case MotionEvent.ACTION_POINTER_UP:
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
-                            .getLayoutParams();
-                    layoutParams.leftMargin = X - _xDelta;
-                    layoutParams.topMargin = Y - _yDelta;
-                    //layoutParams.rightMargin = -250;
-                    //layoutParams.bottomMargin = -250;
-                    view.setLayoutParams(layoutParams);
-                    break;
-            }
-            rootLayout.invalidate();
+
             return true;
         }
-    }
+    }*/
 }
