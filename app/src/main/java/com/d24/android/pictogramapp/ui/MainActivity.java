93fa6b2e27901e5a9db52625da761b88026f421b
Idar Syslak <idasys1@outlook.com>
2017-05-28 23:41:34 +0200

    Create custom layout for displaying stories with buttons in MainActivity

--- app/src/main/java/com/d24/android/pictogramapp/ui/MainActivity.java
+++ app/src/main/java/com/d24/android/pictogramapp/ui/MainActivity.java
@@ -1,8 +1,10 @@
 package com.d24.android.pictogramapp.ui;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +14,7 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,9 +25,12 @@ import java.text.DateFormat;
 import java.util.Date;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SaveDialogFragment.SaveDialogListener {
 
 	private static final int SHOW_INTRODUCTION = 1;
+
+	private ArrayAdapter<File> mAdapter;
+	private ListView mContainer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +38,57 @@ public class MainActivity extends AppCompatActivity {
 
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		toolbar.setTitle(R.string.title_stories);
 		setSupportActionBar(toolbar);
 
 		File internalStorage = getFilesDir();
 		final File[] files = internalStorage.listFiles();
 
-		ArrayAdapter<File> adapter = new ArrayAdapter<File>(this, android.R.layout.simple_list_item_2, android.R.id.text1, files) {
+		mAdapter = new ArrayAdapter<File>(this, R.layout.list_item_file, R.id.text1, files) {
 			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
+			public View getView(int position, View convertView, @NonNull ViewGroup parent) {
 				View view = super.getView(position, convertView, parent);
-				TextView text1 = (TextView) view.findViewById(android.R.id.text1);
-				TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+				TextView text1 = (TextView) view.findViewById(R.id.text1);
+				TextView text2 = (TextView) view.findViewById(R.id.text2);
+				ImageView edit = (ImageView) view.findViewById(R.id.edit);
+				ImageView delete = (ImageView) view.findViewById(R.id.delete);
 
 				DateFormat formatter = DateFormat.getDateInstance();
 				text1.setText(files[position].getName());
 				text2.setText(formatter.format(new Date(files[position].lastModified())));
+				edit.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						SaveDialogFragment dialog = new SaveDialogFragment();
+						// TODO set current filename as argument
+						dialog.show(getFragmentManager(), "save");
+					}
+				});
+				delete.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						// TODO show confirmation
+					}
+				});
+
 				return view;
 			}
 		};
 
-		ListView listView = (ListView) findViewById(R.id.list_files);
-		listView.setAdapter(adapter);
-		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		mContainer = (ListView) findViewById(R.id.list_files);
+		mContainer.setAdapter(mAdapter);
+		mContainer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 				Intent intent = new Intent(MainActivity.this, StagingActivity.class);
-				TextView text = (TextView) view.findViewById(android.R.id.text1);
+				TextView text = (TextView) view.findViewById(R.id.text1);
 				intent.putExtra("filename", (text.getText().toString()));
 				startActivity(intent);
 			}
 		});
 
 		View empty = findViewById(R.id.empty);
-		listView.setEmptyView(empty);
+		mContainer.setEmptyView(empty);
 
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
@@ -123,5 +147,15 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onDialogPositiveClick(DialogFragment dialog, String filename) {
+		File internalStorage = getFilesDir();
+	}
+
+	public void onConfirmationPositiveClick(DialogFragment dialog, int index) {
+		mAdapter.remove(mAdapter.getItem(index));
+		mAdapter.notifyDataSetChanged();
 	}
 }
